72c666ea7dbddfe2691299ef6abdde41163cdcfc
nikhilnayakudi <nayakudinikhil3@gmail.com>
2026-03-17 17:31:12 +0530

    Last and final commit

--- src/main/java/com/CI/CD/CICD/pipeline/Controller.java
+++ src/main/java/com/CI/CD/CICD/pipeline/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("u")
     public String gethello(){
-        return "hello Nikhil,Iam Rohit from mumbai Indians vs Chennai super kings both won 5 trophies";
+        return "hello Nikhil,Iam Rohit from mumbai Indians vs Chennai super kings both are strong teams";
     }
 }
